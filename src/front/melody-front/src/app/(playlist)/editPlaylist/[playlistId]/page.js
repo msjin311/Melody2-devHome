64990fe49ed51f464efcdb87b550c47dfde92dd3
msjin311 <136568295+msjin311@users.noreply.github.com>
2023-10-14 17:54:44 +0900

    editplaylist 작성중

--- src/front/melody-front/src/app/(playlist)/editPlaylist/[playlistId]/page.js
+++ src/front/melody-front/src/app/(playlist)/editPlaylist/[playlistId]/page.js
@@ -1,18 +1,15 @@
 "use client"
 import React, { useEffect, useState, useContext, useTransition } from 'react';
 import Link from 'next/link';
-import {data} from "autoprefixer";
-import  {UserAccountContext}  from "./../../../components/UserAccountContext";
 import { usePathname, useSearchParams } from 'next/navigation'
-import {useUserContext} from "./../../Context/userAccount"
+import {useUserContext} from "../../../Context/userAccount"
 import axios from "axios";
 import Image from "next/image";
-import meatballMenu from '../../img/meatballs-menu.svg'
-import plusImg from "../../img/plus.png";
-import '../../../css/Playlistcss.css'
+import meatballMenu from '../../../img/meatballs-menu.svg'
+import plusImg from "../../../img/plus.png";
+import '../../../../css/Playlistcss.css'
 
-function Playlist   () {
-    // const  userAccount  = useContext(UserAccountContext);
+function EditPlaylist   () {
     const {userAccount,setUserAccount} = useUserContext()
     const userAccountId = userAccount.userAccountId
     const [playlistId, setPlaylistId] = useState(0);
@@ -55,26 +52,7 @@ function Playlist   () {
         playlistHashtags
     };
 
-    // useEffect(() => {
-    //     axios.get(`/api/playlist/${userAccountId}`)
-    //         .then(response =>{
-    //             setPlaylists(response.data)
-    //             console.log(playlists);
-    //         })
-    //         .catch (error =>{
-    //             console.error('playlist 리스트 실패', error);
-    //         });
-    // }, [userAccountId]);
-
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    // const toggleMenu = () => {
-    //     setIsMenuOpen(!isMenuOpen);
-    // };
-    //
-    // const closeMenu = () => {
-    //     setIsMenuOpen(false);
-    // };
 
     useEffect(() => {
         getPlaylistsByUserAccountId()
@@ -113,7 +91,7 @@ function Playlist   () {
 
                 const updatedPlaylists = playlists.filter(playlist => playlist.playlistId !== props);
                 setPlaylists(updatedPlaylists)
-        })
+            })
             .catch(error => {
                 console.error('playlist delelte fail', error)
             })
@@ -128,24 +106,12 @@ function Playlist   () {
                     <h1><b>내 라이브러리</b></h1>
                 </div>
                 <div className="col-span-1 flex justify-end space-x-4">
-                    {/*Playlist Header*/}
-                    {/*<div>*/}
-                    {/*    <button onClick={openModal}></button>*/}
-                    {/*    {isModalOpen && (*/}
-                    {/*        <div className="modal">*/}
-                    {/*            <div className="modal-content">*/}
-                    {/*                <Link href="/createPlaylist"><Image alt="add image" src={plusImg} width={50} height={50}></Image></Link>*/}
-                    {/*            </div>*/}
-                    {/*        </div>*/}
-                    {/*    )}*/}
-                    {/*</div>*/}
                     <Link href="/createPlaylist"><Image alt="noimage" src={plusImg} width={50} height={50}></Image></Link>
-
                 </div>
             </header> {/*<button onClick={closeModal}>Close Modal</button>*/}
 
 
-            <h1>Playlists Page</h1>
+            <h1>Edit Playlists Page</h1>
 
             <div>
                 <ul>
@@ -159,7 +125,6 @@ function Playlist   () {
                                 >
                                     <p>{playlist.playlistName}</p>
                                 </Link>
-                                {/*<button aria-haspopup="menu" data-testid="more-button" className="Button-sc-1dqy6lx-0 jVIpUU" aria-label="내 플레이리스트 #2에 대한 추가 옵션" data-encore-id="buttonTertiary" aria-expanded="false"><span aria-hidden="true" className="IconWrapper__Wrapper-sc-16usrgb-0 hYdsxw"><svg role="img" height="32" width="32" aria-hidden="true" viewBox="0 0 24 24" data-encore-id="icon" className="Svg-sc-ytk21e-0 haNxPq"><path d="M4.5 13.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm15 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm-7.5 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path></svg></span></button>*/}
 
                                 {/*popup*/}
                                 <div className="button-wrapper">
@@ -186,20 +151,8 @@ function Playlist   () {
                     ))}
                 </ul>
             </div>
-
-            {/*<button type="button" onClick={() => router.push('/createPlaylist')}>*/}
-            {/*    create playlist*/}
-            {/*</button>*/}
-
-            {/*<Router>*/}
-            {/*    <Switch>*/}
-            {/*        <Route path="/" exact component={Home} />*/}
-            {/*        <Route path="/about" component={createPlaylist} />*/}
-            {/*        <Route path="/about" component={regifrom} />*/}
-            {/*    </Switch>*/}
-            {/*</Router>*/}
         </>
     );
 }
 
-export default Playlist;
+export default EditPlaylist;
